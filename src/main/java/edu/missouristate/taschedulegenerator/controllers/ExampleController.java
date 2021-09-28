260c8d3e5ceea35c66596ca48e80fd98e8f7dd38
Noah Geren <noah.s.geren@gmail.com>
2021-09-28 17:50:32 -0500

    Replaced example with FXML example

---
+++ src/main/java/edu/missouristate/taschedulegenerator/controllers/ExampleController.java
@@ -0,0 +1,14 @@
+package edu.missouristate.taschedulegenerator.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ExampleController {
+	
+	@FXML
+	public void buttonClicked(ActionEvent event) {
+		System.out.println("Hello World!");
+	}
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module edu.missouristate.taschedulegenerator {
 	requires transitive javafx.controls;
+	requires javafx.fxml;
 	requires static lombok;
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.databind;
 	exports edu.missouristate.taschedulegenerator;
+	exports edu.missouristate.taschedulegenerator.controllers;
 }
