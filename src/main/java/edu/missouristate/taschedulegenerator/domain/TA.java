73a7565fa9d3f19e6d077659c1319eee0727d32c
Corey Rusher <Rusher9@live.missouristate.edu>
2021-10-07 21:48:58 -0500

    updated Course.java and TA.java to utilize Lombok

--- src/main/java/edu/missouristate/taschedulegenerator/domain/TA.java
+++ src/main/java/edu/missouristate/taschedulegenerator/domain/TA.java
@@ -1,38 +1,18 @@
 package edu.missouristate.taschedulegenerator.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TA {
     public String name;
     public boolean isGA;
     public int maxHours;
-    public List<TimeBlock> notAvailable;
-
-    TA(String name, boolean isGA, int maxHours, List<TimeBlock> notAvailable){
-        this.name = name;
-        this.isGA = isGA;
-        this.maxHours = maxHours;
-        this.notAvailable = notAvailable;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public boolean getIsGA(){
-        return this.isGA;
-    }
-    public void setIsGA(boolean isGA){
-        this.isGA = isGA;
-    }
-    public int getMaxHours(){return this.maxHours;}
-    public void setMaxHours(int maxHours){
-        this.maxHours = maxHours;
-    }
-    public List<TimeBlock> getNotAvailable(){
-        return this.notAvailable;
-    }
-    public void setNotAvailable(List<TimeBlock> notAvailable){this.notAvailable = notAvailable;}
+    public List<TimeBlock> notAvailable = new ArrayList<>();
 }
