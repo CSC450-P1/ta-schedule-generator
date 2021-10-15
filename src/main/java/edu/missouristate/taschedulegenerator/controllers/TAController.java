13137c886c02cce4c19f624489cc5aae60e51e16
Corey Rusher <Rusher9@live.missouristate.edu>
2021-10-15 14:03:10 -0500

    Added cancel button to TA view, started US02 task 4

--- src/main/java/edu/missouristate/taschedulegenerator/controllers/TAController.java
+++ src/main/java/edu/missouristate/taschedulegenerator/controllers/TAController.java
@@ -2,16 +2,29 @@ package edu.missouristate.taschedulegenerator.controllers;
 
 import edu.missouristate.taschedulegenerator.SceneManager;
 import edu.missouristate.taschedulegenerator.domain.TA;
+import edu.missouristate.taschedulegenerator.SceneManager.Controller;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
-public class TAController {
+public class TAController implements Controller<String>{
     @FXML private TextField taName;
     @FXML private TextField maxHours;
     @FXML private ToggleGroup isGA;
+    @FXML private TableView timeblockTable;
+
+    @Override
+    public void initData(String data) {
+        // This is where you would process data before showing the view
+        taName.setText(data);
+    }
+
+    @FXML
+    public void cancel(ActionEvent event) {
+        // This is an example of how to switch scenes without passing data
+        SceneManager.showScene("dashboard");
+    }
 
     @FXML
     public void submitTAInfo(ActionEvent event) {
@@ -28,5 +41,7 @@ public class TAController {
         }else{
             newTA.setGA(true);
         }
+
+        ObservableList<TableColumn> columns = timeblockTable.getColumns();
     }
 }
