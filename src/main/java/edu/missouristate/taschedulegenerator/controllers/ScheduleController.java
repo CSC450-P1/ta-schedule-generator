32368beed5446b84fd4099864225b6df1af31a6f
CarlosIzag <67172169+CarlosIzag@users.noreply.github.com>
2021-11-29 19:00:31 -0600

    More changes to loading screen

--- src/main/java/edu/missouristate/taschedulegenerator/controllers/ScheduleController.java
+++ src/main/java/edu/missouristate/taschedulegenerator/controllers/ScheduleController.java
@@ -1,6 +1,8 @@
 package edu.missouristate.taschedulegenerator.controllers;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -10,15 +12,19 @@ import edu.missouristate.taschedulegenerator.domain.Schedule.ScheduledTA;
 import edu.missouristate.taschedulegenerator.util.AppData;
 import edu.missouristate.taschedulegenerator.util.SceneManager;
 import edu.missouristate.taschedulegenerator.util.SceneManager.Controller;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.Pane;
 
-public class ScheduleController implements Controller<Void> {
+public class ScheduleController implements Controller<Void>, Initializable{
 	
 	private List<Schedule> schedules;
 	
@@ -44,8 +50,85 @@ public class ScheduleController implements Controller<Void> {
 	}
 	
 	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		taTable.setEditable(true);
+		//TA Table
+		final TableColumn<Schedule.ScheduledTA, String> taColumn = new TableColumn<>("TA");
+		taColumn.setCellFactory(TextFieldTableCell.<Schedule.ScheduledTA>forTableColumn());
+		taColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(cell.getValue().getTA().getName());
+		});
+		taTable.getColumns().add(taColumn);
+
+		final TableColumn<Schedule.ScheduledTA, String> maxHoursColumn = new TableColumn<>("Max Hours");
+		maxHoursColumn.setCellFactory(TextFieldTableCell.<Schedule.ScheduledTA>forTableColumn());
+		maxHoursColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(String.valueOf(cell.getValue().getTA().getMaxHours()));
+		});
+		taTable.getColumns().add(maxHoursColumn);
+
+		final TableColumn<Schedule.ScheduledTA, String> assignedHoursColumn = new TableColumn<>("Assigned Hours");
+		assignedHoursColumn.setCellFactory(TextFieldTableCell.<Schedule.ScheduledTA>forTableColumn());
+		assignedHoursColumn.setCellValueFactory(cell -> {
+			int assignedHours = 0;
+			for(final ScheduledActivity activity : cell.getValue().getActivities()){
+				assignedHours += activity.getHours();
+			}
+			return new SimpleStringProperty(String.valueOf(assignedHours));
+		});
+		taTable.getColumns().add(assignedHoursColumn);
+
+		final TableColumn<Schedule.ScheduledTA, String> courseColumn = new TableColumn<>("Courses");
+		courseColumn.setCellFactory(TextFieldTableCell.<Schedule.ScheduledTA>forTableColumn());
+		courseColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(
+				cell.getValue().getActivities()
+						.stream()
+						.map(a -> String.format("%s %s", a.getActivity().getCourse().getCourseCode(), a.getActivity().getName()))
+						.collect(Collectors.joining(", ")));
+		});
+		taTable.getColumns().add(courseColumn);
+
+		//Course Table
+		final TableColumn<Schedule.ScheduledActivity, String> courseTableColumn = new TableColumn<>("Course");
+		courseTableColumn.setCellFactory(TextFieldTableCell.<ScheduledActivity>forTableColumn());
+		courseTableColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(cell.getValue().getActivity().getCourse().getCourseCode());
+		});
+		courseTable.getColumns().add(courseTableColumn);
+
+		final TableColumn<Schedule.ScheduledActivity, String> activityColumn = new TableColumn<>("Activity");
+		activityColumn.setCellFactory(TextFieldTableCell.<ScheduledActivity>forTableColumn());
+		activityColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(cell.getValue().getActivity().getName());
+		});
+		courseTable.getColumns().add(activityColumn);
+
+		final TableColumn<Schedule.ScheduledActivity, String> hoursNeededColumn = new TableColumn<>("Hours Needed");
+		hoursNeededColumn.setCellFactory(TextFieldTableCell.<ScheduledActivity>forTableColumn());
+		hoursNeededColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(String.valueOf(cell.getValue().getActivity().getHoursNeeded()));
+		});
+		courseTable.getColumns().add(hoursNeededColumn);
+
+		final TableColumn<Schedule.ScheduledActivity, String> hoursAssignedColumn = new TableColumn<>("Hours Assigned");
+		hoursAssignedColumn.setCellFactory(TextFieldTableCell.<ScheduledActivity>forTableColumn());
+		hoursAssignedColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(String.valueOf(cell.getValue().getHours()));
+		});
+		courseTable.getColumns().add(hoursAssignedColumn);
+
+		final TableColumn<Schedule.ScheduledActivity, String> taTableColumn = new TableColumn<>("TA");
+		taTableColumn.setCellFactory(TextFieldTableCell.<ScheduledActivity>forTableColumn());
+		taTableColumn.setCellValueFactory(cell -> {
+			return new SimpleStringProperty(cell.getValue().getTA().getName());
+		});
+		courseTable.getColumns().add(taTableColumn);
+
+	}
+	
+	@Override
 	public void initData(Void data) {
-		// TODO: Show loading here
 		loadingPane.setVisible(true);
 		System.out.println("Started Generating Schedules");
 		final long startTime = System.currentTimeMillis();
