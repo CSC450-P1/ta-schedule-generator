7be8d18c521822a4e06de90d9753f0997393577b
Noah Geren <noah.s.geren@gmail.com>
2021-12-08 19:56:40 -0600

    Small genetic algorithm improvement

--- src/main/java/edu/missouristate/taschedulegenerator/algorithm/GeneticAlgorithm.java
+++ src/main/java/edu/missouristate/taschedulegenerator/algorithm/GeneticAlgorithm.java
@@ -207,11 +207,11 @@ public class GeneticAlgorithm implements Runnable, Comparator<int[]> {
 	 */
 	private void crossover(final int[][] population) {
 		for(int i = 0; i < population.length - ELITE_COUNT; i++) {
-			if(i > ELITE_COUNT) { // Do crossover
+			if(i >= ELITE_COUNT) { // Do crossover
 				generateOffspring(population[i], selectParent(population), CROSSOVER_RATE); // Use half of each parent's genes
 			} else { // Do crossover but replace an individual on the opposite end of the population
 				population[population.length - i - 1] = Arrays.copyOf(population[i], population[i].length);
-				generateOffspring(population[population.length - i - 1], selectParent(population), 0.5);
+				generateOffspring(population[population.length - i - 1], selectParent(population), CROSSOVER_RATE);
 			}
 		}
 	}
@@ -258,13 +258,8 @@ public class GeneticAlgorithm implements Runnable, Comparator<int[]> {
 	 * @see MUTATION_RATE
 	 */
 	private void mutate(final int[][] population) {
-		for(int i = 0; i < population.length - ELITE_COUNT; i++) {
-			if(i > ELITE_COUNT) { // Do mutation
-				generateOffspring(population[i], getRandomSchedule(), MUTATION_RATE); // Copy genes from a new random schedule
-			} else { // Do mutation but replace an individual on the opposite end of the population
-				population[population.length - i - 1] = Arrays.copyOf(population[i], population[i].length);
-				generateOffspring(population[population.length - i - 1], getRandomSchedule(), MUTATION_RATE);
-			}
+		for(int i = ELITE_COUNT; i < population.length - ELITE_COUNT; i++) {
+			generateOffspring(population[i], getRandomSchedule(), MUTATION_RATE); // Copy genes from a new random schedule
 		}
 	}
 	
